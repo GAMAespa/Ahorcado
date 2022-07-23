45572e74ae274eb26410411acedb41298e662a89
Gabriel Estupiñan <gamaespa@gmail.com>
2022-07-23 13:25:37 -0500

    Creacion del juego ahorcado

---
+++ Juego_ahorcado.py
@@ -0,0 +1,36 @@
+import os
+import random
+
+
+def compare(letter,chosen_word, guess ):
+    if letter in chosen_word:
+        
+        for i,x in enumerate(chosen_word):
+            if x == letter:
+                guess.insert(i, x)
+                guess.pop(i + 1)
+    return guess
+
+
+def run():
+    os.system("cls")
+    with open("./archivos/data.txt", "r", encoding= "utf-8") as word:
+        words_list = [i for i in word]
+    run = True
+    guess = []
+    chosen_word = words_list[random.randint(0, len(words_list) - 1)]
+    chosen_word = list(chosen_word)
+    chosen_word.pop(len(chosen_word) - 1)
+    for i in chosen_word:
+            guess.append("-")
+    while chosen_word != guess:
+        print("".join(guess))
+        chosen_letter = input("Escribe una letra: ")
+        compare(chosen_letter, chosen_word, guess)
+    if chosen_word == guess:
+        print("felicitaciones ganaste")
+        print("".join(guess))
+
+
+if __name__ == '__main__':
+    run()
