d14ec5ff6e85c48a3711ab68f03cf12f9c7d2b5c
Gabriel Estupiñan <gamaespa@gmail.com>
2022-07-23 16:01:28 -0500

    Solucionando la funcion volver a jugar que retornaba error

--- Juego_ahorcado.py
+++ Juego_ahorcado.py
@@ -36,41 +36,60 @@ def run():
     with open("./archivos/data.txt", "r", encoding= "utf-8") as word:
         words_list = [i for i in word]
     guess = []
-    chosen_word = words_list[random.randint(0, len(words_list) - 1)]
-    chosen_word = list(chosen_word)
+    run = True
+    chosen_word = list(words_list[random.randint(0, len(words_list) - 1)])
     chosen_word.pop(len(chosen_word) - 1)
     lives = len(chosen_word) + 5
     for i in chosen_word:
             guess.append("-")
-    while chosen_word != guess:
-        print(Back.RED + Fore.BLACK + ahorcado_image_ascci + Fore.RESET + Back.RESET)
-        print(Fore.RED + "".join(guess))
-        chosen_letter = input("Escribe una letra: ")
-        compare(chosen_letter, chosen_word, guess)
-        lives -= 1
-    if chosen_word == guess:
-        print("felicitaciones ganaste")
-        print("".join(guess))
-        play = int(input("Quieres volver a jugar?: \n (1)Si (2)No, salir"))
-        if play == 1:
-            run()
-        elif play == 2: 
-            run = False
+    while run:
+        if chosen_word != guess and lives > 0:
+            os.system("cls")
+            print(Back.RED + Fore.BLACK + ahorcado_image_ascci + Fore.RESET + Back.RESET)
+            print(Fore.RED + "".join(guess) + "      " + str(lives) + "\u2764")
+            chosen_letter = input("Escribe una letra: ")
+            compare(chosen_letter, chosen_word, guess)
+            lives -= 1
+        elif chosen_word == guess:
+            print("felicitaciones ganaste")
+            print("La palabra era " + "".join(guess))
+            try:
+                play = int(input("Quieres volver a jugar?: \n (1)Si (2)No, salir    "))
+            except ValueError:
+                print("Sea serio")
+                play = 2
+            if play == 1:
+                chosen_word = list(words_list[random.randint(0, len(words_list) - 1)])
+                chosen_word.pop(len(chosen_word) - 1)
+                lives = len(chosen_word) + 5
+                guess = []
+                for i in chosen_word:
+                    guess.append("-")
+            elif play == 2: 
+                run = False
+            else:
+                play
         else:
-            play
-    else:
-        print("perdiste")
-        try:
-            play = int(input("Quieres volver a jugar?: \n (1)Si (2)No, salir"))
-        except ValueError:
-            print("Sea serio")
-            play = 2
-        if play == 1:
-            run()
-        elif play == 2: 
-            run = False
-        else:
-            play
+            print("perdiste")
+            print("La palabra era " + "".join(guess))
+            print("".join(chosen_word))
+            try:
+                play = int(input("Quieres volver a jugar?: \n (1)Si (2)No, salir   "))
+            except ValueError:
+                print("Sea serio")
+                play = 2
+            if play == 1:
+                chosen_word = list(words_list[random.randint(0, len(words_list) - 1)])
+                chosen_word.pop(len(chosen_word) - 1)
+                lives = len(chosen_word) + 5
+                guess = []
+                for i in chosen_word:
+                    guess.append("-")
+                
+            elif play == 2: 
+                run = False
+            else:
+                play
 
 
 if __name__ == '__main__':
